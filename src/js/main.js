df945da66b067ad30cc6f9523fe1e17a858f364b
cecldwll <thaumaturgy101@gmail.com>
2024-09-21 12:28:31 -0600

    nps 1

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,24 @@
 import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
+
+const disclaimerLink = document.querySelector('.disclaimer a');
+disclaimerLink.textContent = parkData.fullName;
+disclaimerLink.href = parkData.url;
+
+function parkInfoTemplate(info) {
+    return `<a href="/" class="hero-banner__title">${info.name}</a>
+    <p class="hero-banner__subtitle">
+      <span>${info.designation}</span>
+      <span>${info.states}</span>
+    </p>`;
+  }
+
+document.title = parkData.name;
+
+const heroImage = document.querySelector('.hero-banner img');
+heroImage.src = parkData.images[0].url;
+heroImage.alt = parkData.images[0].altText;
+
+const heroContent = document.querySelector('.hero-banner__content');
+heroContent.innerHTML = parkInfoTemplate(parkData);
