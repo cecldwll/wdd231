461c3cf350f609b2aa43761603fdf286de537562
cecldwll <thaumaturgy101@gmail.com>
2024-09-24 11:20:42 -0600

    json practice

--- unit1/main.js
+++ unit1/main.js
@@ -3,3 +3,25 @@ import { getRecipes } from "./recipes.mjs";
 const cookieData = getRecipes();
 
 console.log(cookieData);
+
+function setCookieInfo(data) {
+    console.log(data);
+    const container = document.querySelector(".recipe-container");
+    // multiple items in an array to loop through all of them, we use the .map() method
+    const html = data.recipes.map(recipeTemplate);
+    container.innerHTML = html.join("");
+}
+
+
+
+function recipeTemplate(info) {
+    return
+    `
+    <div class="recipe">
+        <h2>${info.recipe_name}</h2>
+        <img src="${info.image}" alt="${info.notes}"/>
+    </div>
+    `
+}
+
+setCookieInfo(cookieData);
