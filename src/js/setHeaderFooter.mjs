e3dbe9233ef7f1c0c6552850bb0f8093e09b0eaa
cecldwll <thaumaturgy101@gmail.com>
2024-09-27 23:01:05 -0600

    nps2

---
+++ src/js/setHeaderFooter.mjs
@@ -0,0 +1,25 @@
+// import templates for park info and footer
+import { parkInfoTemplate, footerTemplate } from "./templates.mjs";
+
+// set header information like disclaimer, page title, and hero banner
+function setHeaderInfo(data) {
+  const disclaimer = document.querySelector(".disclaimer > a");
+  disclaimer.href = data.url; 
+  disclaimer.innerHTML = data.fullName; 
+  document.querySelector("head > title").textContent = data.fullName; 
+  document.querySelector(".hero-banner > img").src = data.images[0].url; 
+  document.querySelector(".hero-banner__content").innerHTML =
+    parkInfoTemplate(data); 
+}
+
+// set footer information
+function setFooter(data) {
+  const footerEl = document.querySelector("#park-footer");
+  footerEl.insertAdjacentHTML("afterbegin", footerTemplate(data)); // add footer content
+}
+
+// set both header and footer using park data
+export default function setHeaderFooter(parkData) {
+  setHeaderInfo(parkData); 
+  setFooter(parkData); 
+}
