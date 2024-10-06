ae49d333f71caa625d2fb4c562ab7ecce3ce7f5d
Caitlyn Caldwell <cecldwll@gmail.com>
2024-10-05 20:57:22 -0600

    nps part 3 - working with api

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,16 @@
 // importing functions and data from other files
-import { getParkData, parkInfoLinks } from "./parkService.mjs";
+import { getParkData, getInfoLinks } from "./parkService.mjs";
 import setHeaderFooter from "./setHeaderFooter.mjs";
 import { mediaCardTemplate } from "./template.mjs";
 
-// getting park data
-const parkData = getParkData();
+// // getting park data
+// const parkData = getParkData();
 
 // display park name and description
 function setParkIntro(data) {
   const introEl = document.querySelector(".intro");
-  introEl.innerHTML = `<h1>${parkData.fullName}</h1>
-  <p>${parkData.description}</p>`;
+  introEl.innerHTML = `<h1>${data.fullName}</h1>
+  <p>${data.description}</p>`;
 }
 
 // display park info links (media cards)
@@ -20,11 +20,21 @@ function setParkInfoLinks(data) {
   infoEl.insertAdjacentHTML("afterbegin", html.join(""));
 }
 
-// set header and footer
-setHeaderFooter(parkData);
+// // set header and footer
+// setHeaderFooter(parkData);
 
-// set park introduction
-setParkIntro(parkData);
+// // set park introduction
+// setParkIntro(parkData);
 
-// set park info links
-setParkInfoLinks(parkInfoLinks);
+// // set park info links
+// setParkInfoLinks(parkInfoLinks);
+
+async function init() {
+  const parkData = await getParkData();
+  const links = getInfoLinks(parkData.images);
+  setHeaderFooter(parkData);
+  setParkIntro(parkData);
+  setParkInfoLinks(links);
+}
+
+init();
